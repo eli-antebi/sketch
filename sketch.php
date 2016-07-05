846de97c315cf28129427f94633278d3190b209b
Eli Antebi <eli.antebi@live.com>
2016-07-05 12:31:25 +0300

    Add files via upload

--- sketch.php
+++ sketch.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sketch
  * Version: 1.0
  * Plugin URI:
- * Description: Adds /sketch folder that allows you to upload sketches and view inside the browser
+ * Description: Allows you to upload images and send your client link to see them in a browser window with popular resolutions guide lines
  * Author: Eli Antebi
  * Author URI: http://eli.antebi.net/
  */
@@ -18,7 +18,7 @@ add_theme_support('post-thumbnails');
 function sketch_template_loader($original_template)
 {
 	if (get_query_var("post_type") == "sketch") {
-		/*if (is_archive() || is_search()) {
+		if (is_archive() || is_search()) {
 			if (file_exists(get_stylesheet_directory() . "/archive-sketch.php")) {
 				return get_stylesheet_directory() . "/archive-sketch.php";
 			} else {
@@ -30,12 +30,6 @@ function sketch_template_loader($original_template)
 			} else {
 				return plugin_dir_path(__FILE__) . "frontend/single-sketch.php";
 			}
-		}*/
-
-		if (is_archive() || is_search()) {
-			return plugin_dir_path(__FILE__) . "frontend/archive-sketch.php";
-		} else {
-			return plugin_dir_path(__FILE__) . "frontend/single-sketch.php";
 		}
 	}
 	else{
@@ -44,6 +38,3 @@ function sketch_template_loader($original_template)
 }
 
 add_action("template_include", "sketch_template_loader");
-
-
-
